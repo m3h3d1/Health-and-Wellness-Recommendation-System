5de095a4284ab2f4eba0e71555b38ac74bc5fec0
Mehedi Hasan (Parvez) <141901188+mehedibjit@users.noreply.github.com>
2023-10-06 12:36:49 +0600

    FeedbackProgress Service Updated

--- FeedbackProgress/src/main/java/com/healthapp/feedbackprogress/dto/ProgressInsightDTO.java
+++ FeedbackProgress/src/main/java/com/healthapp/feedbackprogress/dto/ProgressInsightDTO.java
@@ -3,9 +3,17 @@ package com.healthapp.feedbackprogress.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 public class ProgressInsightDTO {
+    private UUID userId;
+    private Integer age;
+    private String gender;
+    private Integer goalWeight;
+    private Integer targetPeriod;
+
     private String heightInsight;
     private String weightInsight;
     private String diabetesInsight;
