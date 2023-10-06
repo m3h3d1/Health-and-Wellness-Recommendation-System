b43fbd21370354d3adef360510c5ea8b36db7fe3
Mehedi Hasan (Parvez) <141901188+mehedibjit@users.noreply.github.com>
2023-10-06 18:36:18 +0600

    Fixed Security of Feedback Progress Service

--- FeedbackProgress/src/main/java/com/healthapp/feedbackprogress/utilities/token/JWTUtils.java
+++ FeedbackProgress/src/main/java/com/healthapp/feedbackprogress/utilities/token/JWTUtils.java
@@ -16,6 +16,12 @@ public class JWTUtils {
     private static final Random RANDOM = new SecureRandom();
     private static final String ALPHABET = TokenConstants.ALPHABETS;
 
+    /**
+     * Checks if a given JWT token has expired.
+     *
+     * @param token The JWT token to check.
+     * @return True if the token has expired, false otherwise.
+     */
     public static Boolean hasTokenExpired(String token){
         Claims claims = Jwts.parser().setSigningKey(TokenConstants.TOKEN_SECRET).parseClaimsJws(token).getBody();
         Date tokenExpirationDate = claims.getExpiration();
@@ -23,6 +29,13 @@ public class JWTUtils {
         return tokenExpirationDate.before(today);
     }
 
+    /**
+     * Generates a JWT token with the provided user ID and roles.
+     *
+     * @param id    The user ID to include in the token.
+     * @param roles The list of user roles to include in the token.
+     * @return The generated JWT token.
+     */
     public static String generateToken(String id, List<String> roles){
         return Jwts.builder()
                 .setSubject(id)
@@ -32,6 +45,12 @@ public class JWTUtils {
                 .compact();
     }
 
+    /**
+     * Generates a random user ID of the specified length.
+     *
+     * @param length The length of the generated user ID.
+     * @return The generated user ID.
+     */
     public static String generateUserID(int length){
         return generateRandomString(length);
     }
@@ -43,10 +62,22 @@ public class JWTUtils {
         return new String(returnValue);
     }
 
+    /**
+     * Extracts the user ID from a JWT token.
+     *
+     * @param token The JWT token from which to extract the user ID.
+     * @return The user ID extracted from the token.
+     */
     public static String extractUser(String token) {
         return Jwts.parser().setSigningKey(TokenConstants.TOKEN_SECRET).parseClaimsJws(token).getBody().getSubject();
     }
 
+    /**
+     * Extracts the user roles from a JWT token.
+     *
+     * @param token The JWT token from which to extract the user roles.
+     * @return The list of user roles extracted from the token.
+     */
     public static List<String> extractUserRoles(String token) {
         Claims claims = Jwts.parser().setSigningKey(TokenConstants.TOKEN_SECRET).parseClaimsJws(token).getBody();
         return  (List<String>) claims.get("roles");
