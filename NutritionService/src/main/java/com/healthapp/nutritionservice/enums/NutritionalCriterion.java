e335ff2756778516553be13d140f4613099897cc
Mehedi Hasan (Parvez) <141901188+mehedibjit@users.noreply.github.com>
2023-10-06 12:56:15 +0600

    Updated the code of NutritionService for recommendation

---
+++ NutritionService/src/main/java/com/healthapp/nutritionservice/enums/NutritionalCriterion.java
@@ -0,0 +1,13 @@
+package com.healthapp.nutritionservice.enums;
+
+public enum NutritionalCriterion {
+    VITAMIN,
+    PROTEIN,
+    SUGAR,
+    CALORIE,
+    SODIUM,
+    FIBER,
+    FAT,
+    MINERALS,
+    CARBOHYDRATES
+}
