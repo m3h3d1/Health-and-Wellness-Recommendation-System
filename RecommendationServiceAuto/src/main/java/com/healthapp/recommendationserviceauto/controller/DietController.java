ae9e9028c5ef37a38295c1d194740038cfdb51d4
shartaz-bjit <shartaz.yeasar@bjitacademy.com>
2023-10-07 10:58:49 +0600

    If-exists API's added

--- RecommendationServiceAuto/src/main/java/com/healthapp/recommendationserviceauto/controller/DietController.java
+++ RecommendationServiceAuto/src/main/java/com/healthapp/recommendationserviceauto/controller/DietController.java
@@ -5,6 +5,7 @@ import com.healthapp.recommendationserviceauto.model.DietRecommendationDto;
 import com.healthapp.recommendationserviceauto.model.SleepRecommendationDto;
 import com.healthapp.recommendationserviceauto.service.DietService;
 import com.healthapp.recommendationserviceauto.service.SleepService;
+import jakarta.ws.rs.Path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,5 +28,10 @@ public class DietController {
     @GetMapping("/{recommendId}")
     public ResponseEntity<DietRecommendation> getRecommendationById(@PathVariable UUID recommendId){
         return new ResponseEntity<>(dietService.getRecommendationById(recommendId), HttpStatus.OK);
+    }
+    @GetMapping("/recommend/if-exists/{recommendId}")
+    public ResponseEntity<Boolean> checkIfRecordExists(@PathVariable UUID recommendId){
+        return dietService.ifExists(recommendId) ?
+                new ResponseEntity<Boolean>(true, HttpStatus.OK) : new ResponseEntity<Boolean>(false, HttpStatus.NOT_FOUND);
     }
 }
